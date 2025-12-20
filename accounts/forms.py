9f90ac51717fc51aba9a193f3f99f584cf76163e
jihadnassar <jha0211747@ju.edu.jo>
2025-12-21 02:47:21 +0300

    Improve registration form styling

--- accounts/forms.py
+++ accounts/forms.py
@@ -8,3 +8,11 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ("username", "email")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for name, field in self.fields.items():
+            field.widget.attrs.update({
+                "class": "form-control",
+                "placeholder": field.label,
+            })
