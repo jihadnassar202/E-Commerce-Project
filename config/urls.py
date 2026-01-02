22369b2e83143d3dc4c3104a279bcb795bfeddb4
jihadnassar <jha0211747@ju.edu.jo>
2026-01-02 21:59:40 +0300

    Add language switcher to navbar

--- config/urls.py
+++ config/urls.py
@@ -19,8 +19,13 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls.i18n import i18n_patterns
+from django.views.i18n import set_language
 
-urlpatterns = i18n_patterns(
+urlpatterns = [
+    path('setlang/', set_language, name='set_language'),
+]
+
+urlpatterns += i18n_patterns(
     path("admin/", admin.site.urls),
     path("", include("core.urls")),
     path("products/", include("products.urls")),
